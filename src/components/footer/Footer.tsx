a4318bf0cdc401c13d60d1ea4845e7612bc00925
Mahfujul-hoque <mdmahfujulhoque123@gmail.com>
2024-06-28 01:23:00 +0800

    final-code

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Mahfuj</span>
+      <span>© Mahfuj Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
